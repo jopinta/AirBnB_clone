502e72552f368079dcfd0d9af0ace31768a9d353
jopinta <tatinfa@yahoo.com>
2021-11-11 09:03:28 -0800

    do_quit() takes 1 positional argument but 2 were given

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" entry point of the command interpreter
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """class definition"""
+
+    def do_quit(self, arg):
+        """ to quit
+"""
+        return 1
+
+    def do_EOF(self):
+        """end of file exit
+"""
+        return 1
+prompt = "(hbnb) "
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
