96d16ac446e5817b9ddffdbb387be52ac25eb242
Federico Blengio <71684276+fedeblengio@users.noreply.github.com>
2021-11-12 14:30:47 -0300

    gracias aython mi rey :D

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python3
 """module of BaseModel class"""
 
-import models
-from uuid import uuid4
 from datetime import datetime
+import uuid
+import models
 
-class BaseModel:
+
+class BaseModel():
     """ BaseModel """
 
     def __init__(self, *args, **kwargs):
-        """class constructor"""
+        """Class constructor"""
         date = "%Y-%m-%dT%H:%M:%S.%f"
 
         if not kwargs:
             self.created_at = datetime.now()
+            self.id = str(uuid.uuid4())
             self.updated_at = datetime.now()
-            self.id = str(uuid4())
             models.storage.new(self)
         else:
             for key, value in kwargs.items():
                 if key == "updated_at" or key == "created_at":
-                    res = datetime.strptime(value, date)
+                    value = datetime.strptime(value, date)
                 if key != "__class__":
-                    setattr(self, key, res)
+                    setattr(self, key, value)
 
     def __str__(self):
         """ STR method for BaseModel"""
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                self.id, self.__dict__))
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """ Serializes objects to the JSON file """
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ Dictionary containing keys/values """
         dicc = dict(self.__dict__)
-        dicc.update({"__class__": self.__class__.__name__,
-                     "created_at": str(((self.created_at).isoformat())),
-                     "updated_at": str(((self.updated_at).isoformat()))})
+        dicc["created_at"] = self.created_at.isoformat()
+        dicc["__class__"] = self.__class__.__name__
+        dicc["updated_at"] = self.updated_at.isoformat()
         return dicc
-
-    def save(self):
-        """ save """
-        self.updated_at = datetime.now()
-        models.storage.save()
