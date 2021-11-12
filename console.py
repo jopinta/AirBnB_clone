afd8a494ace4d10105c27c5d562704b2a7c7f772
Federico Blengio <71684276+fedeblengio@users.noreply.github.com>
2021-11-12 17:09:53 -0300

    arreglado show y create

--- console.py
+++ console.py
@@ -1,62 +1,57 @@
 #!/usr/bin/python3
 """ entry point of the command interpreter
 """
-
+from shlex import split
 import cmd
 from models.base_model import BaseModel
 import models
 import sys
 
 newdict = {"BaseModel": BaseModel}
-args = []
-
 
 class HBNBCommand(cmd.Cmd):
-    """class definition"""
+    """ Class definition """
 
     prompt =  "(hbnb) "
-    def do_quit(self, arg):
-        """Quit command to exit the program
-"""
+    def do_quit(self, argument):
+        """ Quit command to exit the program """
         return 1
 
     def do_EOF(self):
-        """end of file exit
-"""
+        """ End of file exit """
         return 1
 
-    def do_create(self, arg):
-        """reates a new instance"""
-        args = arg.split(" ")
-        if args == 0:
+    def do_create(self, argument):
+        """ Reates a new instance """
+        args = split(argument)
+        if (len(args)) == 0:
             print ("** class name missing **")
-        if args[0] == "BaseModel":
+        elif args[0] == "BaseModel":
             instancia = BaseModel()
             instancia.save()
             print (instancia.id)
         else:
             print ("** class doesn't exist **")
 
-    def do_show(self, arg):
-        """Prints the string representation"""
-        args = arg.split(" ")
+    def do_show(self, argument):
+        """ Prints the string representation """
+        args = split(argument)
         if (len(args)) == 0:
             print ("** class name missing **")
-
-        if args[0] not in newdict:
+        elif args[0] not in newdict:
             print ("** class doesn't exist **")
         else:
             if (len(args)) == 1:
                     print ("** instance id missing **")
             else:
-                    element = arg[0] + "." + arg[1]
-                    if element in models.storage.all():
+                    element = args[0] + "." + args[1]
+                    if str(element) in models.storage.all():
                             print (models.storage.all()[element])
                     else:
                             print("** no instance found **")
     
     def emptyline(self):
-        ''' empty line '''
+        """ empty line """
         pass
         
 if __name__ == '__main__':
