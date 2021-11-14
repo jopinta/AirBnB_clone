230a8a7e421f649e144b55f7c215160188b09d08
jopinta <tatinfa@yahoo.com>
2021-11-14 08:09:42 -0800

    importing model

--- models/review.py
+++ models/review.py
@@ -3,14 +3,10 @@
 """
 
 from models.base_model import BaseModel
-
+import models
 
 class Review(BaseModel):
 
     place_id = ""
     user_id = ""
     text = ""
-
-    def __init__(self, *args, **kwargs):
-        '''Class Constructor'''
-        super().__init__(*args, **kwargs)
