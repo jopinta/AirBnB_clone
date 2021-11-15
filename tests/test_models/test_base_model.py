cbe2f09563a86044a7f5740c07bb662ebc3f845d
Federico Blengio <fedeeblengio@gmail.com>
2021-11-15 04:22:51 -0800

    save method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-'''Unitest for base_model'''
+""" Unitest for base_model """
 import unittest
 from models.base_model import BaseModel
 
 
 class TestBase(unittest.TestCase):
-    """Test Cases"""
+    """ Test Cases """
     def test_Base(self):
-        '''Test'''
+        """ Test """
         a = BaseModel()
         a.name = "Fede"
         self.assertEqual(a.name, "Fede")
+
+    def test_save(self):
+        """ Test """
+        m = BaseModel()
+        created_at_a = m.created_at
+        updated_at_a = m.updated_at
+        m.save()
+        created_at_b = m.created_at
+        updated_at_b = m.updated_at
+        self.assertEqual(created_at_a, created_at_b)
+        self.assertNotEqual(updated_at_a, updated_at_b)
 
 
 if __name__ == "__main__":
